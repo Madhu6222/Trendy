61a4c46e469032ec7993b77fd4ceca91261f56b8
madhukar paladugu <madhukar6222@gmail.com>
2020-11-06 20:38:46 +0530

    Added Products and Categories Business Entities and Created Database.

---
+++ Trendy.Database/TrendyDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Trendy.Entities;
+
+namespace Trendy.Database
+{
+    public class TrendyDbContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        public TrendyDbContext() :base("TrendyConnection")
+        {
+
+        }
+    }
+}
