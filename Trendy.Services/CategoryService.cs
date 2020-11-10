3e07dcf90331d66d08e5737810c36d1a9110a447
madhukar paladugu <madhukar6222@gmail.com>
2020-11-10 20:12:40 +0530

    CRUD for Categories and Products added and Ajax for Products.

---
+++ Trendy.Services/CategoryService.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Trendy.Database;
+using Trendy.Entities;
+
+namespace Trendy.Services
+{
+    public class CategoryService
+    {
+
+        public Category GetCategory(int ID)
+        {
+            using (var context = new TrendyDbContext())
+            {
+                return context.Categories.Find(ID);
+            }
+        }
+
+        public List<Category> GetCategories()
+        {
+            using (var context = new TrendyDbContext())
+            {
+                return context.Categories.ToList();
+            }
+        }
+
+        public void SaveCategory(Category category) 
+        {
+            using (var context = new TrendyDbContext()) 
+            {
+                context.Categories.Add(category);
+                context.SaveChanges();
+            }
+        }
+
+        public void UpdateCategory(Category category)
+        {
+            using (var context = new TrendyDbContext())
+            {
+                context.Entry(category).State = System.Data.Entity.EntityState.Modified;
+                context.SaveChanges();
+            }
+        }
+
+        public void DeleteCategory(int ID)
+        {
+            using (var context = new TrendyDbContext())
+            {
+                // context.Entry(category).State = System.Data.Entity.EntityState.Deleted;
+                var category = context.Categories.Find(ID);
+                context.Categories.Remove(category);
+                context.SaveChanges();
+            }
+        }
+    }
+}
---
+++ Trendy.Services/ProductsService.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Trendy.Database;
+using Trendy.Entities;
+
+namespace Trendy.Services
+{
+    public class ProductsService
+
+    {
+
+        public Product GetProduct(int ID)
+        {
+            using (var context = new TrendyDbContext())
+            {
+                return context.Products.Find(ID);
+            }
+        }
+
+        public List<Product> GetProducts()
+        {
+            using (var context = new TrendyDbContext())
+            {
+                return context.Products.ToList();
+            }
+        }
+
+        public void SaveProduct(Product product) 
+        {
+            using (var context = new TrendyDbContext()) 
+            {
+                context.Products.Add(product);
+                context.SaveChanges();
+            }
+        }
+
+        public void UpdateProduct(Product product)
+        {
+            using (var context = new TrendyDbContext())
+            {
+                context.Entry(product).State = System.Data.Entity.EntityState.Modified;
+                context.SaveChanges();
+            }
+        }
+
+        public void DeleteProduct(int ID)
+        {
+            using (var context = new TrendyDbContext())
+            {
+                // context.Entry(product).State = System.Data.Entity.EntityState.Deleted;
+                var product = context.Products.Find(ID);
+                context.Products.Remove(product);
+                context.SaveChanges();
+            }
+        }
+    }
+}
